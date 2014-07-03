a3e05905b067b6211378e460cded8f9d042fd13a
Paul Wenig <pwenig@gmail.com>
2014-07-03 11:48:03 -0600

    Fixed user login when registered

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:id] = @user.id
+      session[:user_id] = @user.id
       flash[:register_message] = "Welcome, #{@user.name}"
       redirect_to "/"
     else
